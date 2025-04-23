07c46e2af73870a8e5596b6b56075b44b2985b32
dluqueru <dluqueru@gmail.com>
2025-04-23 20:41:40 +0200

    arreglo responsividad y estilos en el componente nav

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,19 +1,54 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, AfterViewInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../auth/services/auth.service';
 import { DefaultImageDirective } from '../directives/default-image.directive';
 
 @Component({
   selector: 'app-nav',
+  standalone: true,
   imports: [RouterLink, DefaultImageDirective],
   templateUrl: './nav.component.html'
 })
-export class NavComponent {
+export class NavComponent implements AfterViewInit {
+  authService = inject(AuthService);
+  router = inject(Router);
+  isMobileMenuOpen = false;
+  private mobileMenu: HTMLElement | null = null;
 
-  authService: AuthService = inject(AuthService);
-
-  logout(){
-    this.authService.logout();
+  isLoginRoute(): boolean {
+    return this.router.url === '/login';
   }
 
+  ngAfterViewInit() {
+    const mobileMenuButton = document.getElementById('mobileMenuButton');
+    this.mobileMenu = document.getElementById('mobileMenu');
+
+    if (mobileMenuButton && this.mobileMenu) {
+      mobileMenuButton.addEventListener('click', () => this.toggleMobileMenu());
+    }
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    
+    if (this.mobileMenu) {
+      if (this.isMobileMenuOpen) {
+        this.mobileMenu.style.maxHeight = this.mobileMenu.scrollHeight + 'px';
+      } else {
+        this.mobileMenu.style.maxHeight = '0';
+      }
+    }
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+    if (this.mobileMenu) {
+      this.mobileMenu.style.maxHeight = '0';
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+    this.closeMobileMenu();
+  }
 }
